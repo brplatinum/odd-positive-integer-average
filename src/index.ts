07082f5bc048d530a2f20ad6335c061daad9de09
Reece White <reece.white@cloudsense.com>
2021-04-21 17:43:31 +1000

    Made it PORT not port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { initExpressApp } from './app';
 
 const app = initExpressApp();
-const port = process.env.port ?? 8080;
+const port = process.env.PORT ?? 8080;
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
